101e551d4ee2a10c49f1ee203b4902a2a3dff240
Alfredo Magnani Sakatauskas <alfmagnanis@gmail.com>
2021-06-18 01:09:39 -0300

    Feito desenvolvimento do server necessário para a apresentação.

--- server/src/library/data-sources/books.js
+++ server/src/library/data-sources/books.js
@@ -3,7 +3,7 @@ const { RESTDataSource } = require('apollo-datasource-rest');
 class BooksAPI extends RESTDataSource {
     constructor() {
         super();
-        this.baseURL = 'http://localhost:3001';
+        this.baseURL = 'http://localhost:3000';
     }
 
     async getBooks() {
@@ -12,6 +12,46 @@ class BooksAPI extends RESTDataSource {
             ...book,
             author: await this.get(`/authors/${book.author}`)
         }));
+    }
+
+    async getBook(id) {
+        const book = await this.get(`/books/${id}`);
+        book.author = await this.get(`/authors/${book.author}`);
+
+        return book;
+    }
+
+    async addBook(book) {
+        const books = await this.get(`/books`);
+        const id = ++books.length;
+        const currentDate = new Date();
+        await this.post(`/books`, { 
+            ...book,
+            id
+        });
+
+        return ({
+            id,
+            message: "Livro cadastrado com sucesso!"
+        })
+    }
+
+    async updateBook(id, book) {
+        this.put(`/books/${id}`, { ...book, id });
+
+        return ({
+            id,
+            message: "Livro atualizado com sucesso!"
+        })
+    }
+
+    async deleteBook(id) {
+        await this.delete(`/books/${id}`);
+
+        return ({
+            id,
+            message: "Livro excluido com sucesso!"
+        })
     }
 }
 
--- server/src/library/resolvers.js
+++ server/src/library/resolvers.js
@@ -1,6 +1,29 @@
+const { GraphQLScalarType } = require('graphql');
+
 const resolvers = {
+    DateTime: new GraphQLScalarType({
+        name: 'DateTime',
+        description: 'String de data e hora no formato ISO-8601',
+        serialize: value => {
+            const date = new Date(value);
+            return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
+        },
+        parseValue: value => new Date(value),
+        parseLiteral: ast => new Date(ast.value)
+    }),
+    DefaultResponse: {
+        __resolveType(obj, context, info) {
+            return false
+        }
+    },
     Query: {
-        books: (parent, args, { dataSources }, info) => dataSources.booksAPI.getBooks()
+        books: (parent, args, { dataSources }, info) => dataSources.booksAPI.getBooks(),
+        book: (parent, { id }, { dataSources }, info) => dataSources.booksAPI.getBook(id)
+    },
+    Mutation: {
+        addBook: (parent, { book, createdAt }, { dataSources }, info) => dataSources.booksAPI.addBook({ ...book, createdAt}),
+        updateBook: (parent, { id, book }, { dataSources }, info) => dataSources.booksAPI.updateBook(id, book),
+        deleteBook: (parent, { id }, { dataSources }, info) => dataSources.booksAPI.deleteBook(id)
     }
 };
 
